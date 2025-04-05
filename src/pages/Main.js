80dd16cf1a6a9b7cb96d446d78a3d308dbe47384
de0k <104269755+de0k@users.noreply.github.com>
2025-04-06 01:39:13 +0900

    리액트 라우트 테스트

---
+++ src/pages/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Main 입니다</h1>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/pages/Sub01.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sub01 = () => {
+  return (
+    <div>
+      <h1>서브페이지 /sub01 입니다</h1>
+    </div>
+  );
+};
+
+export default Sub01;
